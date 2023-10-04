f5e16ee915a857f502503eb08980eb0a49bd5209
Matias Cancina <127862158+MatiasCancina@users.noreply.github.com>
2023-10-04 20:51:02 -0300

    Update ToDos.tsx

--- src/components/ToDos.tsx
+++ src/components/ToDos.tsx
@@ -1,1 +1,50 @@
+import { ToDo } from './ToDo'
+import type { ToDo as ToDoType } from '../types'
+import { useState } from 'react'
+import { useAutoAnimate } from '@formkit/auto-animate/react'
 
+interface Props {
+  toDos: ToDoType[]
+  setCompleted: (id: string, completed: boolean) => void
+  setTitle: (params: Omit<ToDoType, 'completed'>) => void
+  removeTodo: (id: string) => void
+}
+
+export const ToDos: React.FC<Props> = ({
+  toDos,
+  setCompleted,
+  setTitle,
+  removeTodo
+}) => {
+  const [isEditing, setIsEditing] = useState('')
+  const [parent] = useAutoAnimate()
+
+  return (
+    <ul className="todo-list" ref={parent}>
+      {toDos?.map(toDo => (
+        <li
+          key={toDo.id}
+          onDoubleClick={() => {
+            setIsEditing(toDo.id)
+          }}
+          className={`
+            ${toDo.completed ? 'completed' : ''}
+            ${isEditing === toDo.id ? 'editing' : ''}
+          `}
+        >
+          <ToDo
+            key={toDo.id}
+            id={toDo.id}
+            title={toDo.title}
+            completed={toDo.completed}
+            setCompleted={setCompleted}
+            setTitle={setTitle}
+            removeToDo={removeTodo}
+            isEditing={isEditing}
+            setIsEditing={setIsEditing}
+          />
+        </li>
+      ))}
+    </ul>
+  )
+}
